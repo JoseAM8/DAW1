d66b58fdc9228a286e0a44361c2c117df65798ac
José Alunda Morte <joal7509@proven.local>
2023-10-02 15:00:18 +0200

    push

---
+++ M03-UF1/HorasTrabajadas/src/horastrabajadas/HorasTrabajadas.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package horastrabajadas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author joal7509
+ */
+public class HorasTrabajadas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int horas, tarnorm, tarextr;
+        
+        tarnorm = 20;
+        tarextr = 30;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Cuantas horas has laburado? ");
+        horas = sc.nextInt();
+        
+        int horasextr=horas-35, calc=horasextr*tarextr, suma=calc+700;
+        
+        if (horas<=35) {
+            System.out.println("Cobraras " + horas*tarnorm + " Euros");
+        }
+        
+        else if (horas>35) {
+            System.out.println("Cobraras " + suma + " Euros");
+        }
+        
+        else {
+            System.out.println("Ha ocrrido un error");
+        }
+    }
+    
+}
